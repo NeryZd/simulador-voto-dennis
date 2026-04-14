a54225418d7756343e1c66b87e79930cced6cbab
NeryZd <neryzdg@gmail.com>
2026-04-14 09:51:26 -0300

    Version 1
    Beta 1.0 Simulador de votos Dennis.

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'simulador-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './appcheba.jpg',
+  './appdennis.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Responder desde el caché
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
